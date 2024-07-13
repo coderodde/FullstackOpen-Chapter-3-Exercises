c76755608253f8d015e0b04f0b66274d3c818172
Rodion Efremov <coderodd3@yandex.com>
2024-07-13 13:19:38 +0300

    ...

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
-require('dotenv').config()
-
 const express = require('express')
 const app = express();
-const morgan = require('morgan')
-const cors = require('cors');
+require('dotenv').config()
 
 const Person = require('./models/person')
 
+const requestLogger = (request, response, next) => {
+    console.log("Method:", request.method)
+    console.log("Path: :", request.path)
+    console.log("Body  :", request.body)
+    console.log("---")
+    next()
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+    console.log("Error:", error)
+
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "Malformed ID." })
+    }
+
+    next(error)
+}
+
+const cors = require('cors');
+//const morgan = require('morgan')
+
 app.use(cors())
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
 
-morgan.token('body', req => {
-    return JSON.stringify(req.body)
-}) 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "Unknown endpoint." })
+}
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+// morgan.token('body', req => {
+//     return JSON.stringify(req.body)
+// }) 
 
-app.get('/api/persons', (request, response) => {
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(persons => {
         response.json(persons)
     })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
-})
-
-app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people <br/>${new Date()}`)
-})
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name) {
@@ -48,12 +63,6 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (persons.find(p => p.name === body.name)) {
-        return response.status(400).json({
-            error: `${body.name} is already in the phonebook.`
-        })
-    }
-
     const person = new Person({
         name: body.name,
         number: body.number,
@@ -62,9 +71,29 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
+
+app.get('/info', (request, response, next) => {
+    Person.find({}).then(result => {
+        response.send(`Phonebook has info for ${result.length} people <br/>${new Date()}`)
+    })
+    .catch(error => next(error))
+})
+
+
+app.delete('/api/persons/:id', (request, response, next ) => {
     Person.findByIdAndDelete(request.params.id)
     .then(result => {
         response.status(204).end()
@@ -72,7 +101,25 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}!`)
 })

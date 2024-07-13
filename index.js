6fc44b9eac8c2609011105cf87a2956e00ea4462
Rodion Efremov <coderodd3@yandex.com>
2024-07-13 11:21:08 +0300

    Exercise 3.15 done.

--- index.js
+++ index.js
@@ -7,27 +7,6 @@ const cors = require('cors');
 
 const Person = require('./models/person')
 
-//const url = "mongodb+srv://coderodd3:Exp10r1ngM0ng0D8@fullstackopenmongodbclu.uw53f8u.mongodb.net/people?retryWrites=true&w=majority&appName=FullstackOpenMongoDBCluster"
-// const Person = require('./models/person');
-
-// mongoose.set('strictQuery', false)
-// mongoose.connect(url)
-
-// const personSchema = new mongoose.Schema({
-//     name: String,
-//     number: String,
-// })
-
-// const Person = mongoose.model('Person', personSchema)
-
-// personSchema.set('toJSON', {
-//     transform: (document, returnedObject) => {
-//         returnedObject.id = returnedObject._id.toString()
-//         delete returnedObject._id
-//         delete returnedObject.__v
-//     }
-// })
-
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
@@ -37,8 +16,6 @@ morgan.token('body', req => {
 }) 
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-let persons = []
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
@@ -50,17 +27,6 @@ app.get('/api/persons/:id', (request, response) => {
     Person.findById(request.params.id).then(person => {
         response.json(person)
     })
-
-    // const id = request.params.id
-    // const person = persons.find(p => p.id === id)
-
-    // if (person) {
-    //     response.send(`${person.name}, ${person.number}`)
-    // } else {
-    //     response.status(404).json({
-    //         error: "Content missing!"
-    //     })
-    // }
 })
 
 app.get('/info', (request, response) => {
@@ -98,10 +64,12 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(p => p.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
